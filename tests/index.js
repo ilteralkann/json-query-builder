55cdee994b2eea0f9ccedf0524e9349bca662220
İlter <ilteralkan@hotmail.com>
2021-06-03 19:53:14 +0300

    added errorHandlers

--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,17 @@
 const jqb = require('../lib/jqb')
 
-jqb.json = './test.json';
-
 /*
-jqb.insert({
-    "jqbTest":"12345",
-    "jqbAsdf":"878990"
-}) // Inserts obj to JSON.
+jqb.insert('test.json', ({
+    "x":"2"
+})) // Inserts obj to JSON.
 
 
  jqb.select("json path") // Returns selected JSON (FULL DATA)
  jqb.select("json path", "users12") // Returns selected JSON in "user12" value.
  
+jqb.update('test.json', 'testss', 'TESTASADAsss') // Updates selected paths, selected value.
+
+
+jqb.delete('./test.json','test2') // Deletes selected files selected row.
 */
+
