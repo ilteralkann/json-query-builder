f09c16f7bcc6b0d8c2e16d0f7aa0d9578acca658
İlter <ilteralkan@hotmail.com>
2021-06-03 14:45:30 +0300

    Initial commit

---
+++ tests/index.js
@@ -0,0 +1,15 @@
+const jqb = require('../lib/jqb')
+
+jqb.json = './test.json';
+
+/*
+jqb.insert({
+    "jqbTest":"12345",
+    "jqbAsdf":"878990"
+}) // Inserts obj to JSON.
+
+
+ jqb.select("json path") // Returns selected JSON (FULL DATA)
+ jqb.select("json path", "users12") // Returns selected JSON in "user12" value.
+ 
+*/
